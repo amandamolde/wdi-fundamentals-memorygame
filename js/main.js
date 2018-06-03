5471d5bf84620dd3c032316d91ef3ca2f1162c02
Amanda Molde <amanda.molde@gmail.com>
2018-06-03 12:22:17 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -8,7 +8,31 @@ console.log("User flipped " + cardOne);
 console.log("User flipped " + cardFour);
 */
 
-var cards = ["queen", "queen", "king", "king"];
+/*var cards = ["queen", "queen", "king", "king"];*/
+
+var cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images.queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images.queen-of-diamonds.png"
+
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images.king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images.king-of-diamonds.png"
+}
+];
 
 var cardsInPlay = [];
 
@@ -32,9 +56,11 @@ var flipCard = function(cardId) {
 	console.log("User flipped king");
 */
 
-	console.log("User flipped " + cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 
-	cardsInPlay.push(cards[cardId]);
+	cardsInPlay.push(cards[cardId].rank);
 
 	checkForMatch();
 
